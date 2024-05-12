d6adac2a9f061a2a1305b28b7e3b420408e82eb2
Colin Scruggs <colinscruggs97@gmail.com>
2024-05-12 17:33:05 -0500

    Initialize pg Pool to connect to database

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express'); 
 const cors = require('cors');
 const morgan = require('morgan');
+const db = require('./db');
 
 const app = express();
 const port = process.env.PORT || 3001;
